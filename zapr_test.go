8f10272781f8608e490b5d062797bc547283b0a6
Santiago Verdu <erra@qmake.org>
2022-11-28 12:39:30 -0600

    luyc

--- zapr_test.go
+++ zapr_test.go
@@ -4,6 +4,14 @@ import "testing"
 
 func TestTopLevelLogs(t *testing.T) {
   Init()
-  I("Initialized successfully")
+  D("Debug")
+  I("Info")
+  W("Warn")
+  E("Error")
   t.Log("Initialized and logged successfully")
+}
+
+func TestVLevel(t *testing.T) {
+  // Idk how to do this properly
+  V(5).I("Test")
 }
