a6e3a871c06cf53a6aa20102426ca47c5520c123
Santiago Verdu <erra@qmake.org>
2022-11-28 10:20:34 -0600

    y are u eating chicken

--- zapr.go
+++ zapr.go
@@ -2,6 +2,7 @@ package zapr
 
 import (
   "go.uber.org/zap"
+  "go.uber.org/zap/zapcore"
 )
 
 const (
@@ -14,15 +15,64 @@ type settings struct { }
 type loggingT struct {
   settings
   vLevel uint8
-  mode uint
+  logger *zap.Logger
+}
+
+type bervose struct {
+  enabled bool
+}
+
+func (b bervose) Enabled() bool {
+  return b.enabled
+}
+
+func (b bervose) I(m string, args ...zapcore.Field) { 
+  if b.enabled {
+    logging.logger.Info(m, args...)
+  }
+}
+
+func (b bervose) W(m string, args ...zapcore.Field) {
+  if b.enabled {
+    logging.logger.Warn(m, args...)
+  }
+}
+
+func (b bervose) E(m string, args ...zapcore.Field) {
+  if b.enabled {
+    logging.logger.Error(m, args...)
+  }
+}
+
+func (b bervose) D(m string, args ...zapcore.Field) {
+  if b.enabled {
+    logging.logger.Debug(m, args...)
+  }
 }
 
 var logging loggingT
 
-func Sugar() {
-  logging.mode = ZSugar
+// API
+func I(m string, args ...zapcore.Field) {
+  logging.logger.Info(m, args...)
 }
 
-func Logger() {
-  logging.mode = ZLogger
+func E(m string, args ...zapcore.Field) {
+  logging.logger.Error(m, args...)
+}
+
+func D(m string, args ...zapcore.Field) {
+  logging.logger.Debug(m, args...)
+}
+
+func W(m string, args ...zapcore.Field) {
+  logging.logger.Warn(m, args...)
+}
+
+func Sync() {
+  logging.logger.Sync()
+}
+
+func V(level uint8) *bervose {
+  return &bervose{level <= logging.vLevel}
 }
